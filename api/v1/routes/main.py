37d3ef49f9899ae2c80b60776f4c056bea7b8495
Franklin Ikeh <ikehfranklin06@gmail.com>
2024-09-19 13:29:25 +0100

    add shortener router

--- api/v1/routes/main.py
+++ api/v1/routes/main.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from api.v1.routes.auth import auth
+from api.v1.routes.url_shortener import shorten
 
 main_router = APIRouter(prefix="/api/v1")
 
 main_router.include_router(router=auth)
+main_router.include_router(router=shorten)
