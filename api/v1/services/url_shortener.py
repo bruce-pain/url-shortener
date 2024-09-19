3ea15e1d3e3796eb1e9e4981467dc3a4a8f674e0
Franklin Ikeh <ikehfranklin06@gmail.com>
2024-09-19 13:28:38 +0100

    feat: url shortener service

--- api/v1/services/url_shortener.py
+++ api/v1/services/url_shortener.py
@@ -1,31 +1,76 @@
+import string
+
 from fastapi import HTTPException, status
-from uuid_extensions import uuid7
 from sqlalchemy.orm import Session
+from uuid import UUID
+from uuid_extensions import uuid7
 
-from api.v1.models.short_urls import ShortUrl
 from api.core import response_messages
+from api.v1.models.short_urls import ShortUrl
+from api.v1.models.user import User
+from api.v1.schemas import url_shortener
+
+# Base62 character set
+BASE62 = string.ascii_letters + string.digits
 
 
-def generate_random_short_url(length: int = 8) -> str:
-    uuid = uuid7()
+def encode_base62(num: int) -> str:
+    """Function to encode an integer to a Base62 string
 
-    return uuid.hex[:length]
+    Args:
+        num (int): integer to encode
+
+    Returns:
+        str: Base62 encoded integer
+    """
+    if num == 0:
+        return BASE62[0]
+
+    encoded = []
+    base = len(BASE62)
+
+    while num > 0:
+        encoded.append(BASE62[num % base])
+        num //= base
+
+    # Return the reversed list to form the correct Base62 string
+    return "".join(reversed(encoded))
+
+
+def generate_short_code(length: int = 7) -> str:
+    """Generate a short code from uuid
+
+    Args:
+        length (int, optional): length of the short code. Defaults to 7.
+
+    Returns:
+        str: generated short code
+    """
+    unique_id = uuid7().int
+    encoded_id = encode_base62(unique_id)
+    return encoded_id[-length:]
 
 
 def create_shortened_url(
-    db: Session, length: int, target_url: str, custom_alias: str = None
+    db: Session, schema: url_shortener.CreateShortUrl, current_user: User
 ) -> ShortUrl:
+    target_url = schema.target_url
+    custom_alias = schema.custom_alias
+    length = schema.length
+
     if custom_alias:
         url_string = custom_alias
     else:
-        url_string = generate_random_short_url(length=length)
+        url_string = generate_short_code(length=length)
 
-    existing_url = db.query(ShortUrl).filter(ShortUrl.short_url == url_string).first()
+    existing_url = db.query(ShortUrl).filter(ShortUrl.short_code == url_string).first()
 
     if existing_url:
         raise HTTPException(status_code=400, detail=response_messages.ALIAS_IN_USE)
 
-    short_url = ShortUrl(target_url=target_url, short_url=url_string)
+    short_url = ShortUrl(
+        target_url=target_url, short_code=url_string, user_id=current_user.id
+    )
 
     db.add(short_url)
     db.commit()
@@ -36,7 +81,7 @@ def create_shortened_url(
 
 def get_target_url(db: Session, short_url: str) -> str:
     short_url_object = (
-        db.query(ShortUrl).filter(ShortUrl.short_url == short_url).first()
+        db.query(ShortUrl).filter(ShortUrl.short_code == short_url).first()
     )
 
     return short_url_object.target_url
